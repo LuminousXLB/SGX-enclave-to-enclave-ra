09a24b3f2b84feeeb29e936861a2514bfef94f4a
SHEN Jiamin <shen_jiamin@outlook.com>
2019-08-27 18:29:47 +0800

    init

---
+++ ias_http/app.h
@@ -0,0 +1,19 @@
+//
+// Created by ncl on 27/8/19.
+//
+
+#ifndef SGX_ENCLAVE_TO_ENCLAVE_RA_APP_H
+#define SGX_ENCLAVE_TO_ENCLAVE_RA_APP_H
+
+#include <cstdio>
+#include <curl/curl.h>
+
+void doit();
+
+class IntelAttestationService {
+
+};
+
+
+
+#endif //SGX_ENCLAVE_TO_ENCLAVE_RA_APP_H
---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+
+#define SPID "6E5E7C36753F78EEB9506C784B6E0B87"
+#define PRIMARY_KEY "84262f16cf944a05b7e719cd0b57f8db"
+#define SECONDARY_KEY "cf4adc642a0442a99fd5a0dfc957bc4d"
+
+void doit();
+
+int main() {
+
+    doit();
+
+    return 0;
+}
