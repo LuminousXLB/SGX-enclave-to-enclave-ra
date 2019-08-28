6425081d3159bcd40ce7948674c6f8a9e888a49f
SHEN Jiamin <shen_jiamin@outlook.com>
2019-08-28 14:16:57 +0800

    all files here

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@
 #define PRIMARY_KEY "84262f16cf944a05b7e719cd0b57f8db"
 #define SECONDARY_KEY "cf4adc642a0442a99fd5a0dfc957bc4d"
 
-void doit();
-
 int main() {
 
-    doit();
+//    doit();
 
     return 0;
 }
