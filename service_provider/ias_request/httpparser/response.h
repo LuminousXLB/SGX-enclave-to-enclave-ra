c83a4a2e55f4a1eba39600e17f82d082e7d88b5a
SHEN Jiamin <shen_jiamin@outlook.com>
2019-08-30 18:25:54 +0800

    server

--- service_provider/ias_request/httpparser/response.h
+++ service_provider/ias_request/httpparser/response.h
@@ -3,11 +3,14 @@
  * License: MIT
  */
 
+// Changes [JM] by John Mechalas <john.p.mechalas@intel.com>
+
 #ifndef HTTPPARSER_RESPONSE_H
 #define HTTPPARSER_RESPONSE_H
 
 #include <string>
 #include <vector>
+#include <algorithm> // Added by JM
 #include <sstream>
 
 namespace httpparser
@@ -46,9 +49,44 @@ struct Response {
         }
 
         std::string data(content.begin(), content.end());
-        stream << data << "\n";
+		// Added "\n" so it prints like its received. - JM
+        stream << "\n" << data << "\n";
         return stream.str();
     }
+
+	// content_string() by JM
+	std::string content_string() const
+	{
+        std::stringstream stream;
+
+        std::string data(content.begin(), content.end());
+        stream << data;
+
+		return stream.str();
+	}
+
+	// headers_as_string() by JM
+	std::string headers_as_string(std::string name) const
+	{
+        std::stringstream stream;
+		// Just in case we get more than one header with the same name.
+		for(std::vector<Response::HeaderItem>::const_iterator it = headers.begin();
+            it != headers.end(); ++it)
+		{
+			if ( is_equal_ncase(it->name, name) ) stream << it->value << "\n";
+		}
+
+		return stream.str();
+	}
+
+	// Case-insensitive string comparison - JM
+	bool is_equal_ncase(std::string a, std::string b) const
+	{
+		transform(a.begin(), a.end(), a.begin(), ::toupper);
+		transform(b.begin(), b.end(), b.begin(), ::toupper);
+		return ( a == b );
+	}
+
 };
 
 } // namespace httpparser
